b6188a467493eb5a8c852baa9a4ae800ca584b24
Alexander Luna <alluna@fanatics.com>
2025-10-08 09:39:16 -0500

    fmt: :q :q

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -7,23 +7,23 @@ resource "aws_db_subnet_group" "this" {
 }
 
 resource "aws_db_instance" "postgres" {
-  allocated_storage    = 20
-  engine               = var.rds_engine
-  engine_version       = var.rds_engine_version
-  instance_class       = var.rds_instance_class
-  db_name              = var.rds_db
-  username             = var.rds_db_user
-  password             = var.rds_db_pass
-  db_subnet_group_name = aws_db_subnet_group.this.name
+  allocated_storage      = 20
+  engine                 = var.rds_engine
+  engine_version         = var.rds_engine_version
+  instance_class         = var.rds_instance_class
+  db_name                = var.rds_db
+  username               = var.rds_db_user
+  password               = var.rds_db_pass
+  db_subnet_group_name   = aws_db_subnet_group.this.name
   vpc_security_group_ids = [var.rds_sg_id]
-  skip_final_snapshot  = true
+  skip_final_snapshot    = true
   tags = {
     Name = "${var.rds_tag_name}"
   }
 }
 
 resource "null_resource" "enable_postgis" {
-  count = var.enable_gis ? 1 : 0
+  count      = var.enable_gis ? 1 : 0
   depends_on = [aws_db_instance.postgres]
 
   provisioner "local-exec" {
